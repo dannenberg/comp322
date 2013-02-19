eaad4bb8ff1e42a9a799d409eb386a9a10e3f332
Matt Dannenberg <matt.dannenberg@10gen.com>
2013-02-19 17:39:51 -0500

    white space commit

--- assign2/Parser.cc
+++ assign2/Parser.cc
@@ -7,34 +7,35 @@ using namespace std;
 
 struct Variable
 {
-  string name;
-  double value;
+    string name;
+    double value;
 };
 
 struct Memory
 {
-  vector<string> instructions;
-  int position;
-  vector<Variable> variables;
+    vector<string> instructions;
+    int position;
+    vector<Variable> variables;
 };
 
 
 int main(int argc, char ** argv) {
-  if (argc < 2)
-  {
-    cerr << "Error: No input file specified. Usage Parser <filename>" << endl;
-    return 1;
-  }
+    if (argc < 2)
+    {
+        cerr << "Error: No input file specified. Usage Parser <filename>" << endl;
+        return 1;
+    }
 
-  ifstream in_file;
-  in_file.open(argv[1]);
-  if (!in_file)
-  {
-    cerr << "Error: File not able to be opened" << endl;
-    return 1;
-  }
+    ifstream in_file;
+    in_file.open(argv[1]);
+    if (!in_file)
+    {
+        cerr << "Error: File not able to be opened" << endl;
+        return 1;
+    }
 
-  //parse the file and execute the instructions here!
-  return 0;
+    //parse the file and execute the instructions here!
+    return 0;
+
 }
 
